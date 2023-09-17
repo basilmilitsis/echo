b997587a33cfcd7aac523c052d4ac1bcde4ee058
Bas <basil.mil@gmail.com>
2023-09-17 20:03:00 +0200

    integrated codegen'ed api into domain-api example

--- domain-api/src/index.ts
+++ domain-api/src/index.ts
@@ -1,32 +1,3 @@
 import 'source-map-support/register';
 
-import { v4 as uuidv4 } from 'uuid';
-import { createPost, publishPost, archivePost } from './domain/post/_generated/Post.operations';
-import { DefaultEventStream, EventStream } from './common';
-import { CommandContext } from './common/CommandContext';
-
-const eventStream: EventStream = new DefaultEventStream();
-const context: CommandContext = {eventStream, generateUuid: uuidv4};
-
-const doLogic = async () => {
-    await createPost({
-        id: '12345',
-        text: 'some text',
-        images: []
-    }, context)
-    await publishPost({
-        id: '12345',
-    }, context)
-    await archivePost({
-        id: '12345',
-    }, context)
-}
-
-(async () => {
-    await doLogic();
-})().catch(e => {
-    if(e instanceof Error) console.log(`error: ${e.name}, message: ${e.message}}`);
-    console.log('THREW ERROR');
-});
-
-
+import './_generated/restAPI.generated';
