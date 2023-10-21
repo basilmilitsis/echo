87e49d7a3cfc9c9ba196c07fbef1c65602cd949c
Bas <basil.mil@gmail.com>
2023-10-21 16:08:07 +0200

    builder: removing dependincy on lib-common

--- builder/src/common/BuilderEnvironment.ts
+++ builder/src/common/BuilderEnvironment.ts
@@ -1,5 +1,3 @@
-import { BaseEnvironment } from '@echo/lib-common';
-
-export class BuilderEnvironment extends BaseEnvironment {
-    static pwd: string = BuilderEnvironment.parseString('PWD');
+export class BuilderEnvironment {
+    static pwd: string = process.env.PWD || '';
 }
