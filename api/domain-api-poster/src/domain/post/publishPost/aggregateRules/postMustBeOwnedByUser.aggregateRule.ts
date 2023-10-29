bb26a7cb74f0d73962f0c26ed4ec2ad7f9b2c92d
Bas <basil.mil@gmail.com>
2023-10-29 16:31:07 +0200

    removed old rules

--- api/domain-api-poster/src/domain/post/publishPost/aggregateRules/postMustBeOwnedByUser.aggregateRule.ts
+++
@@ -1,8 +0,0 @@
-import { CommandAggregateRule } from "@echo/lib-domain-api";
-import { Post } from "@root/domain/post/Post";
-import { PublishPost } from "@root/domain/post/publishPost/PublishPost.update.command";
-
-export const postMustBeOwnedByUser: CommandAggregateRule<PublishPost, Post> = (command: PublishPost, aggregate: Post): string[] => {
-    // TODO
-    return [];
-} 
