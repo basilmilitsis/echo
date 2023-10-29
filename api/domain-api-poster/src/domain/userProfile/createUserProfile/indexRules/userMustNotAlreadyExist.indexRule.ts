a6ad7845b61ee720f6182281f5931dc09e9e4dea
Bas <basil.mil@gmail.com>
2023-10-29 15:40:20 +0200

    userProfile index rule - check if user already exists

--- api/domain-api-poster/src/domain/userProfile/createUserProfile/indexRules/userMustNotAlreadyExist.indexRule.ts
+++ api/domain-api-poster/src/domain/userProfile/createUserProfile/indexRules/userMustNotAlreadyExist.indexRule.ts
@@ -1,7 +1,12 @@
-import { EventStream, CommandIndexRule } from "@echo/lib-domain-api";
-import { CreateUserProfile } from "@root/domain/userProfile/createUserProfile/CreateUserProfile.create.command";
+import { EventStream, CommandIndexRule } from '@echo/lib-domain-api';
+import { CreateUserProfile } from '@root/domain/userProfile/createUserProfile/CreateUserProfile.create.command';
 
-export const userMustNotAlreadyExist: CommandIndexRule<CreateUserProfile> = async (command: CreateUserProfile, eventStream: EventStream): Promise<string[]> => {
-    // TODO
+export const userMustNotAlreadyExist: CommandIndexRule<CreateUserProfile> = async (
+    command: CreateUserProfile,
+    eventStream: EventStream
+): Promise<string[]> => {
+    if ((await eventStream.findEvents('userProfile', command.id)).length > 0) {
+        return ['user already exists'];
+    }
     return [];
-}   
+};
