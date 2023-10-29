d368a546471be70f377a44d9b659fba9e2e59b09
Bas <basil.mil@gmail.com>
2023-10-29 16:30:24 +0200

    added an example of auth rules to bootstrapper

--- runme-bootstrap-domain.sh
+++ runme-bootstrap-domain.sh
@@ -7,87 +7,89 @@ cd api/domain-api-poster
 # UserProfile
 # ------------------------------------------------------------------------------------------------
 
-builder api domain add-aggregate        UserProfile 
+builder api domain add-aggregate            UserProfile 
 
 # CreateUserProfile
-builder api domain add-create-command   UserProfile   createUserProfile
+builder api domain add-create-command       UserProfile   createUserProfile
 # ---
-builder api domain add-index-rule       UserProfile   createUserProfile     userMustNotAlreadyExist
+builder api domain add-command-auth-rule    UserProfile   createUserProfile       userIdMustMatchToken
+builder api domain add-index-rule           UserProfile   createUserProfile       userMustNotAlreadyExist
 # ---
-builder api domain add-event            UserProfile   createUserProfile     userProfileCreated
+builder api domain add-event                UserProfile   createUserProfile       userProfileCreated
 
 # ChangeUserProfileName
-builder api domain add-update-command   UserProfile   changeUserProfileName
+builder api domain add-update-command       UserProfile   changeUserProfileName
 # ---
-builder api domain add-index-rule       UserProfile   changeUserProfileName   userProfileMustExist
+builder api domain add-index-rule           UserProfile   changeUserProfileName   userProfileMustExist
 # ---
-builder api domain add-event            UserProfile   changeUserProfileName   userProfileNameChanged
+builder api domain add-event                UserProfile   changeUserProfileName   userProfileNameChanged
 
 # SetUserProfilePicture
-builder api domain add-update-command   UserProfile   setUserProfilePicture
+builder api domain add-update-command       UserProfile   setUserProfilePicture
 # ---
-builder api domain add-index-rule       UserProfile   setUserProfilePicture   userProfileMustExist
+builder api domain add-index-rule           UserProfile   setUserProfilePicture   userProfileMustExist
 # ---
-builder api domain add-event            UserProfile   setUserProfilePicture   userProfilePictureSet
+builder api domain add-event                UserProfile   setUserProfilePicture   userProfilePictureSet
 
 # SetUserProfileLocation
-builder api domain add-update-command   UserProfile   setUserProfileLocation
+builder api domain add-update-command       UserProfile   setUserProfileLocation
 # ---
-builder api domain add-index-rule       UserProfile   setUserProfileLocation   userProfileMustExist
+builder api domain add-index-rule           UserProfile   setUserProfileLocation   userProfileMustExist
 # ---
-builder api domain add-event            UserProfile   setUserProfileLocation   userProfileLocationSet
+builder api domain add-event                UserProfile   setUserProfileLocation   userProfileLocationSet
 
 
 # ------------------------------------------------------------------------------------------------
 # Post
 # ------------------------------------------------------------------------------------------------
 
-builder api domain add-aggregate        Post
+builder api domain add-aggregate            Post
 
 # CreatePost
-builder api domain add-create-command   Post    createPost
+builder api domain add-create-command        Post    createPost
 # ---
-builder api domain add-index-rule       Post    createPost      postMustNotAlreadyExist
-builder api domain add-index-rule       Post    createPost      userMustExist
-builder api domain add-command-rule     Post    createPost      maximumOfTenImages
+builder api domain add-index-rule            Post    createPost      postMustNotAlreadyExist
+builder api domain add-index-rule            Post    createPost      userMustExist
+builder api domain add-command-rule          Post    createPost      maximumOfTenImages
 # ---
-builder api domain add-event            Post    createPost      postCreated
+builder api domain add-event                 Post    createPost      postCreated
 
 # ChangePostText
-builder api domain add-update-command   Post    changePostText
+builder api domain add-update-command        Post    changePostText
 # ---
-builder api domain add-index-rule       Post    changePostText     postMustExist
-builder api domain add-aggregate-rule   Post    changePostText     postMustBeOwnedByUser
-builder api domain add-aggregate-rule   Post    changePostText     postMustNotBeArchived
+builder api domain add-index-rule            Post    changePostText     postMustExist
+builder api domain add-aggregate-rule        Post    changePostText     postMustBeOwnedByUser
+builder api domain add-aggregate-rule        Post    changePostText     postMustNotBeArchived
 # ---
-builder api domain add-event            Post    changePostText     postTitleChanged
+builder api domain add-event                 Post    changePostText     postTitleChanged
 
 # PublishPost
-builder api domain add-update-command   Post    publishPost
+builder api domain add-update-command        Post    publishPost
 # ---
-builder api domain add-index-rule       Post    publishPost     postMustExist
-builder api domain add-aggregate-rule   Post    publishPost     postMustBeOwnedByUser
-builder api domain add-aggregate-rule   Post    publishPost     postMustBeUnpublished
+builder api domain add-aggregate-auth-rule   post    publishPost     mustBeOwnPost
+builder api domain add-index-rule            Post    publishPost     postMustExist
+builder api domain add-aggregate-rule        Post    publishPost     postMustBeOwnedByUser
+builder api domain add-aggregate-rule        Post    publishPost     postMustBeUnpublished
 # ---
-builder api domain add-event            Post    publishPost     postPublished
+builder api domain add-event                 Post    publishPost     postPublished
 
 # UnpublishPost
-builder api domain add-update-command   Post    unpublishPost
+builder api domain add-update-command        Post    unpublishPost
 # ---
-builder api domain add-index-rule       Post    unpublishPost   postMustExist
-builder api domain add-aggregate-rule   Post    unpublishPost   postMustBeOwnedByUser
-builder api domain add-aggregate-rule   Post    unpublishPost   postMustBePublished
+builder api domain add-index-rule            Post    unpublishPost   postMustExist
+builder api domain add-aggregate-rule        Post    unpublishPost   postMustBeOwnedByUser
+builder api domain add-aggregate-rule        Post    unpublishPost   postMustBePublished
 # ---
-builder api domain add-event            Post    unpublishPost   postUnpublished
+builder api domain add-event                 Post    unpublishPost   postUnpublished
 
 # ArchivePost
-builder api domain add-update-command   Post    archivePost
+builder api domain add-update-command        Post    archivePost
 # ---
-builder api domain add-index-rule       Post    archivePost     postMustExist
-builder api domain add-aggregate-rule   Post    archivePost     postMustBeOwnedByUser
-builder api domain add-aggregate-rule   Post    archivePost     postMustNotBeArchived
+builder api domain add-index-rule            Post    archivePost     postMustExist
+builder api domain add-aggregate-rule        Post    archivePost     postMustBeOwnedByUser
+builder api domain add-aggregate-rule        Post    archivePost     postMustNotBeArchived
 # ---
-builder api domain add-event            Post    archivePost     postArchived
+builder api domain add-event                 Post    archivePost     postArchived
 
 # ------------------------------------------------------------------------------------------------
 # PostLikes
